5091b25d3c8a0686899a68420047f149020dc3a8
Anton Zadvorny <thcheetah@gmail.com>
2018-07-31 18:20:22 +0300

    Remove type definitions

--- lib/telegram/bot/types/inline_keyboard_markup.rb
+++
@@ -1,9 +0,0 @@
-module Telegram
-  module Bot
-    module Types
-      class InlineKeyboardMarkup < Base
-        attribute :inline_keyboard, Array.of(Array.of(InlineKeyboardButton))
-      end
-    end
-  end
-end
